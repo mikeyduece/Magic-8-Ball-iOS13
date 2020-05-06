54d4ae9e7b5161463643d4146a148291790bb745
Mike Heft <mikeheft@gmail.com>
2020-05-06 10:06:06 -0600

    Add actions for button press and device shake

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var ballImageView: UIImageView!
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            ballImageView.image = ballArray.randomElement()
+        }
+    }
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballImageView.image = ballArray.randomElement()
+    }
+    
 
 }
 
